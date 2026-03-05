32ab4e29800fbe9cf30c64e35925b9594939214a
ayrathapar <thaparayra07@gmail.com>
2026-03-05 09:47:57 +0530

    UC8: Linked list based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,33 +1,41 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
 
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
         LinkedList<Character> list = new LinkedList<>();
 
-        for(char c : input.toCharArray()) {
+        // Add characters to LinkedList
+        for (char c : input.toCharArray()) {
             list.add(c);
         }
 
         boolean isPalindrome = true;
 
-        while(list.size() > 1) {
-            if(!list.removeFirst().equals(list.removeLast())) {
+        // Compare first and last characters
+        while (list.size() > 1) {
+
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        if(isPalindrome)
+        if (isPalindrome) {
             System.out.println("Result: Palindrome");
-        else
+        } else {
             System.out.println("Result: Not Palindrome");
+        }
 
         scanner.close();
     }
